c7b1b19bc9cd13475a8a9ee981b7c91925bc48c7
Sarthak Sahu <vishalsahu5@gmail.com>
2020-02-16 19:19:39 +0530

    Combination sum 4

---
+++ P377.java
@@ -0,0 +1,25 @@
+class Solution {
+    
+    private int[] dp;
+
+    public int combinationSum4(int[] nums, int target) {
+        dp = new int[target + 1];
+        Arrays.fill(dp, -1);
+        dp[0] = 1;
+        return combinationSumUtil(nums, target);
+    }
+
+    private int combinationSumUtil(int[] nums, int target) {
+        if (dp[target] != -1) {
+            return dp[target];
+        }
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (target >= nums[i]) {
+                res += combinationSumUtil(nums, target - nums[i]);
+            }
+        }
+        dp[target] = res;
+        return res;
+    }
+}
