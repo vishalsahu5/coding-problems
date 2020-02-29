0f05248ba257a09c5f119111429e7f6a61f848e5
Sarthak Sahu <vishalsahu5@gmail.com>
2020-02-29 15:01:36 +0530

    Length of last word

--- P58.java
+++ P58.java
@@ -1,19 +1,5 @@
 class Solution {
-  public int lengthOfLastWord(String str) {
-    if (str.length() == 0) {
-        return 0;
-    }
-    
-    int start = 0;
-    int end = str.length()-1;
-    
-    while (end >= 0 && end >= start && str.charAt(end) == ' ') {
-        end--;
-    }
-    start = end;
-    while (start >= 0 && str.charAt(start) != ' ') {
-        start--;
-    }
-    return end - start;
-}
+  public int lengthOfLastWord(String s) {
+    return s.trim().length()-s.trim().lastIndexOf(" ")-1;
+  }
 }
