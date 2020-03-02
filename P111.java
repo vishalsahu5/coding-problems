abed99b9bb5651f894d1655c77f7ea6d63f2206f
Sarthak Sahu <vishalsahu5@gmail.com>
2020-03-02 23:04:38 +0530

    Minimum Depth of Binary Tree

--- P111.java
+++ P111.java
@@ -8,29 +8,28 @@
  * }
  */
 class Solution {
-    public int minDepth(TreeNode root) {
-        
-        if (root == null) return 0;
-        int result = 0;
-        Queue<TreeNode> q = new LinkedList<>();
-        HashMap<TreeNode, Integer> m = new HashMap<>();
-        q.add(root);
-        m.put(root, 1);
-        while (q.size() != 0) {
-            TreeNode currNode = q.remove();
-            if (currNode.left == null && currNode.right == null) {
-                result = m.get(currNode);
-                break;
-            }
-            if (currNode.left != null) {
-                q.add(currNode.left);
-                m.put(currNode.left, m.get(currNode) + 1);
-            }
-            if (currNode.right != null) {
-                q.add(currNode.right);
-                m.put(currNode.right, m.get(currNode) + 1);
-            }
-        }
-        return result;
-    }
+  public int minDepth(TreeNode root) {
+      
+      if (root == null) return 0;
+      Queue<TreeNode> q = new LinkedList<>();
+      q.add(root);
+      int height = 0;
+      while (q.size() != 0) {
+          height++;
+          int size = q.size();
+          for (int i=0; i < size; i++) {
+              TreeNode currNode = q.remove();
+              if (currNode.left == null && currNode.right == null) {
+                  return height;
+              }
+              if (currNode.left != null) {
+                  q.add(currNode.left);
+              }
+              if (currNode.right != null) {
+                  q.add(currNode.right);
+              }
+          }
+      }
+      return height;
+  }
 }
