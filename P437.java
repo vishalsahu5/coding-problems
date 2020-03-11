58e7cd6dfe942d6a4dfb35b2b7b147ede1a9a4f0
Sarthak Sahu <vishalsahu5@gmail.com>
2020-03-11 21:17:39 +0530

    Path sum III

--- P437.java
+++ P437.java
@@ -9,28 +9,28 @@
  */
 class Solution {
     
-    private HashMap<Integer, Integer> set;
+    private HashMap<Integer, Integer> map;
     private int count;
     
     private void dfs(TreeNode root, int sum, int k) {
         if (root == null) return;
         
         sum += root.val;
-        if (set.containsKey(sum - k)) {
-            count += set.get(sum-k);
-        }
-        set.put(sum, set.getOrDefault(sum, 0) + 1);
+        
+        count += map.getOrDefault(sum-k, 0);
+        
+        map.put(sum, map.getOrDefault(sum, 0) + 1);
         dfs(root.left, sum, k);
         dfs(root.right, sum, k);
-        set.put(sum, Math.max(0, set.getOrDefault(sum, 0)-1));
+        map.put(sum, Math.max(0, map.getOrDefault(sum, 0)-1));
     }
     
     
     public int pathSum(TreeNode root, int sum) {
         if (root == null) return 0;
         count = 0;
-        set = new HashMap<>();
-        set.put(0, 1);
+        map = new HashMap<>();
+        map.put(0, 1);
         dfs(root, 0, sum);
         return count;
     }
