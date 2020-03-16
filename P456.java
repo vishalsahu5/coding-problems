a4a16b9b198ab71c37bcce2831bb923fbc4c8d49
Sarthak Sahu <vishalsahu5@gmail.com>
2020-03-16 18:35:25 +0530

    132 Pattern

--- P456.java
+++ P456.java
@@ -1,20 +1,22 @@
 class Solution {
-    public boolean find132pattern(int[] nums) {
-        if (nums == null || nums.length < 3) return false;
-        int[] minVal = new int[nums.length];
-        
-        minVal[0] = nums[0];
-        for (int i=1; i<nums.length; i++) {
-            minVal[i] = Math.min(minVal[i-1], nums[i]);
-        }
-        
-        for (int j=1; j <= nums.length-2; j++) {
-            for (int k=j+1; k<=nums.length-1; k++) {
-                if (nums[k] < nums[j] && minVal[j-1] < nums[k]) {
-                    return true;
-                }
-            }
-        }
-        return false;
+  public boolean find132pattern(int[] nums) {
+    if (nums == null || nums.length < 3)
+      return false;
+    int[] minVal = new int[nums.length];
+
+    minVal[0] = nums[0];
+    for (int i = 1; i < nums.length; i++) {
+      minVal[i] = Math.min(minVal[i - 1], nums[i]);
     }
+
+    for (int j = 1; j <= nums.length - 2; j++) {
+      for (int k = j + 1; k <= nums.length - 1; k++) {
+        if (nums[k] < nums[j] && minVal[j - 1] < nums[k]) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
 }
+// O(n^2) solution with O(n) extra space
