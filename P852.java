25d29998ba1516ac6931387bd03a57dc7f064641
Sarthak Sahu <vishalsahu5@gmail.com>
2020-03-20 21:55:25 +0530

    Peak Index in a Mountain Array

--- P852.java
+++ P852.java
@@ -1,10 +1,19 @@
 class Solution {
   public int peakIndexInMountainArray(int[] A) {
-    for (int i = 0; i < A.length - 1; i++) {
-      if (A[i] > A[i + 1]) {
-        return i;
+    int start = 0, end = A.length - 1;
+    while (start < end - 1) {
+      int mid = start + (end - start) / 2;
+      if (A[mid] > A[mid + 1] && A[mid] > A[mid - 1]) {
+        return mid;
+      } else if (A[mid] < A[mid + 1]) {
+        start = mid;
+      } else if (A[mid] < A[mid - 1]) {
+        end = mid;
       }
     }
-    return A.length - 1;
+    if (A[start] < A[end]) {
+      return A[end];
+    }
+    return A[start];
   }
 }
