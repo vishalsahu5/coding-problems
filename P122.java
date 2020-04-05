e8012bbacce8683bb91affe64985f957920f9bfe
Sarthak Sahu <vishalsahu5@gmail.com>
2020-04-05 20:42:10 +0530

    Best time to buy and sell stock II

---
+++ P122.java
@@ -0,0 +1,8 @@
+class Solution {
+  public int maxProfit(int[] prices) {
+    int result = 0;
+    for (int i = 1; i < prices.length; i++)
+      result += Math.max(0, prices[i] - prices[i - 1]);
+    return result;
+  }
+}
