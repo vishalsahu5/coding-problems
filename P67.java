1f000f86f23610307d0e479c3c1ea1304cefe9cc
Sarthak Sahu <vishalsahu5@gmail.com>
2020-05-02 15:14:47 +0530

    Add Binary

--- P67.java
+++ P67.java
@@ -1,46 +1,18 @@
-class Solution {
-
-  private String prepend(String s, int zeros) {
-    StringBuilder sb = new StringBuilder(s);
-    sb = sb.reverse();
-    for (int i = 0; i < zeros; i++) {
-      sb.append('0');
-    }
-    return sb.reverse().toString();
-  }
-
+public class Solution {
   public String addBinary(String a, String b) {
-    if (b.length() > a.length())
-      return addBinary(b, a);
-
-    b = prepend(b, a.length() - b.length());
-
-    char carry = '0';
-    StringBuilder result = new StringBuilder();
-    for (int i = b.length() - 1; i >= 0; i--) {
-      char bit1 = a.charAt(i);
-      char bit2 = b.charAt(i);
-      if (bit1 == '0' && bit2 == '0' && carry == '0') {
-        result.append('0');
-        carry = '0';
-      } else if (bit1 == '1' && bit2 == '1' && carry == '1') {
-        result.append('1');
-        carry = '1';
-      } else if (bit1 == '0' && bit2 == '0' && carry == '1') {
-        result.append('1');
-        carry = '0';
-      } else if (bit1 == '1' && bit2 == '0' && carry == '0') {
-        result.append('1');
-      } else if (bit1 == '0' && bit2 == '1' && carry == '0') {
-        result.append('1');
-        carry = '0';
-      } else {
-        result.append('0');
-        carry = '1';
-      }
+    StringBuilder sb = new StringBuilder();
+    int i = a.length() - 1, j = b.length() - 1, carry = 0;
+    while (i >= 0 || j >= 0) {
+      int sum = carry;
+      if (j >= 0)
+        sum += b.charAt(j--) - '0';
+      if (i >= 0)
+        sum += a.charAt(i--) - '0';
+      sb.append(sum % 2);
+      carry = sum / 2;
     }
-    if (carry == '1')
-      result.append('1');
-    return result.reverse().toString();
+    if (carry != 0)
+      sb.append(carry);
+    return sb.reverse().toString();
   }
 }
