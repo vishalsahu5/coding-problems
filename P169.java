f96b0363da4926be453de1af4e1554375ae4ecd4
Sarthak Sahu <vishalsahu5@gmail.com>
2020-05-06 18:27:57 +0530

    Majority Element

---
+++ P169.java
@@ -0,0 +1,18 @@
+class Solution {
+  public int majorityElement(int[] nums) {
+    int majority = nums[0];
+    int count = 1;
+    for (int i = 1; i < nums.length; i++) {
+      if (majority == nums[i]) {
+        count++;
+      } else {
+        count--;
+        if (count == 0) {
+          majority = nums[i];
+          count++;
+        }
+      }
+    }
+    return majority;
+  }
+}
