080f03ca84cce06955a974cb2f3545af4a828217
Sarthak Sahu <vishalsahu5@gmail.com>
2020-05-07 17:51:22 +0530

    Third Maximum Number

--- P414.cpp
+++ P414.cpp
@@ -1,36 +1,18 @@
 #include <algorithm>
 #include <cmath>
 #include <iostream>
+#include <set>
 #include <vector>
 
 using namespace std;
-
 class Solution {
-   private:
-    long excludedMax(vector<int> nums, vector<long> excluded) {
-        long maxval = LONG_MIN;
-        for (int i = 0; i < nums.size(); i++) {
-            if (find(excluded.begin(), excluded.end(), nums[i]) ==
-                excluded.end()) {
-                if (maxval < nums[i]) maxval = nums[i];
-            }
-        }
-        return maxval;
-    }
-
    public:
     int thirdMax(vector<int>& nums) {
-        if (nums.size() < 3) {
-            return *max_element(nums.begin(), nums.end());
+        set<int> top3;
+        for (int num : nums) {
+            top3.insert(num);
+            if (top3.size() > 3) top3.erase(top3.begin());
         }
-        long first = LONG_MIN, second = LONG_MIN, third = LONG_MIN;
-        first = *max_element(nums.begin(), nums.end());
-        vector<long> excluded;
-        excluded.push_back(first);
-        second = excludedMax(nums, excluded);
-        excluded.push_back(second);
-        third = excludedMax(nums, excluded);
-        // cout << first << " " << second << " " << third << endl;
-        return third == LONG_MIN ? first : third;
+        return top3.size() == 3 ? *top3.begin() : *top3.rbegin();
     }
 };
