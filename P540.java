a80b927511b39ef0ccc341c6889cc654f98ce120
Sarthak Sahu <vishalsahu5@gmail.com>
2020-05-12 21:23:49 +0530

    Single element in a sorted array

---
+++ P540.java
@@ -0,0 +1,11 @@
+class Solution {
+  public int singleNonDuplicate(int[] nums) {
+      int n=nums.length, lo=0, hi=n/2;
+      while (lo < hi) {
+          int m = (lo + hi) / 2;
+          if (nums[2*m]!=nums[2*m+1]) hi = m;
+          else lo = m+1;
+      }
+      return nums[2*lo];
+  }
+}
