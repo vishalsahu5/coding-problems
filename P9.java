0f8d78aa6059b4c9ac648cf35788566b90b892d1
Sarthak Sahu <vishalsahu5@gmail.com>
2020-05-21 23:47:22 +0530

    Palindrome Number

--- P9.java
+++ P9.java
@@ -1,3 +1,4 @@
+//Palindrome number
 class Solution {
     public boolean isPalindrome(int x) {
         if (x < 0) {
@@ -6,8 +7,8 @@ class Solution {
         int reverse = 0;
         int temp = x;
         while (temp != 0) {
-            reverse = (10*reverse) + (temp%10);
-            temp = temp/10;
+            reverse = (10 * reverse) + (temp % 10);
+            temp = temp / 10;
         }
         return x == reverse;
     }
