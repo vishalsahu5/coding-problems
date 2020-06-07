6061902870c6755473f172a6f0e62d0b75fbd0ab
Sarthak Sahu <vishalsahu5@gmail.com>
2020-06-07 20:46:45 +0530

    Binary search tree from preorder traversal

--- P1008.java
+++ P1008.java
@@ -1,34 +1,27 @@
 /**
  * Definition for a binary tree node. public class TreeNode { int val; TreeNode
- * left; TreeNode right; TreeNode(int x) { val = x; } }
+ * left; TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
+ * = left; this.right = right; } }
  */
 class Solution {
+  public TreeNode util(int[] preorder, int left, int right) {
+    if (left > right)
+      return null;
+
+    TreeNode root = new TreeNode(preorder[left]);
+    int i;
+    for (i = left; i < preorder.length; i++) {
+      if (preorder[i] > preorder[left])
+        break;
+    }
+    root.left = util(preorder, left + 1, i - 1);
+    root.right = util(preorder, i, right);
+    return root;
+  }
+
   public TreeNode bstFromPreorder(int[] preorder) {
 
     return util(preorder, 0, preorder.length - 1);
   }
-
-  private TreeNode util(int[] preorder, int start, int end) {
-    if (start > end) {
-      return null;
-    }
-    if (start == end) {
-      return new TreeNode(preorder[start]);
-    }
-
-    TreeNode root = new TreeNode(preorder[start]);
-    int mid = start + 1;
-    for (int i = start + 1; i < preorder.length; i++) {
-      if (preorder[i] > preorder[start]) {
-        break;
-      } else {
-        mid++;
-      }
-    }
-
-    root.left = util(preorder, start + 1, mid - 1);
-    root.right = util(preorder, mid, end);
-    return root;
-  }
-
 }
